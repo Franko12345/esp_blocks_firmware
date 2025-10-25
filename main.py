f2ea28a3bb16db95e4e69f38b949f318e4422c61
Franco12345 <franco@delucca.pro.br>
2025-10-25 15:09:31 -0300

    Updated code from esp

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import network
-import usocket as socket
-from request_parser import parse_request
 import esp
 import time
 import _thread
 from machine import Pin
 import machine
+from microdot import Microdot, Response, send_file
+from microdot_cors import CORS
 
 
 machine.freq(240000000)
@@ -32,59 +32,47 @@ def do_connect():
 
 do_connect()
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', 80))
-s.listen(50)
-
 led = Pin(2, Pin.OUT)
 
+app = Microdot()
 
-def recieve_code(conn, args):
-    print(args)
+cors = CORS(
+    app,
+    allowed_origins='*',                     # <-- string '*' (não ['*'])
+    allow_credentials=False,
+    allowed_methods=['GET', 'POST', 'OPTIONS'],
+    allowed_headers=['Content-Type', 'Authorization'],  # recomendo incluir Authorization se for necessário
+    max_age=3600
+)
 
-    conn.send("Code successfully recieved!")
-    conn.close()
+@app.route('/code', methods=["GET", "POST"])
+async def index(request):
+    code = request.body.decode("utf-8")
+    with open("code", "w") as arq:
+        arq.write(code)
+        
+    print(code)
+    return Response("Code Recieved successfully!")
+
+@app.route('/')
+async def index(request):
+    led.value(not led.value())
+    return send_file("main.html")
 
 
-routes = {
-    "/recieve" : recieve_code
-    #"/set-color": set_static_color,
-    #"/dynamic": set_dynamic_animation
-    }
-
-#variables = {
-#    "STATE": lambda: "true" if state == "on" else "false",
-#    "MODE": lambda: str(not active_animation).lower(),
-#    "COLOR": lambda: "#" + open("color.txt", "r").read()
-#}
-
-
-while True:
-    conn, addr = s.accept()
-    request = conn.recv(1024)
-    request = parse_request(request)
-    conn.send("Size recieved".encode("utf-8"))
-    conn.close()
+@app.route('/run', methods=["GET", "POST"])
+async def index(request):
+    try:
+        with open("code", "r") as arq:
+            exec(arq.read())
+        return "Code Executed successfully!"
     
-    conn, addr = s.accept()
-    request = conn.recv(1024 + int(request["args"]["size"]))
-    request = parse_request(request)
-    conn.send(request["body"].encode("utf-8"))
-    conn.close()
-#    try:
-#
-#        request = parse_request(request)
-#
-#        led.toggle()
-#
-#        if request["url"] in routes:
-#            time.sleep(.05)
-#            _thread.start_new_thread(routes[request["url"]], (conn, request["args"]))
-#
-#        else:
-#            conn.send("pong")
-#            conn.close()
-#            
-#    except Exception as e:
-#        conn.send(str(e).encode("utf-8"))
-#        conn.close()
+    except Exception as e:
+        return Response("Error: " + e)
+    
+@app.route('/off', methods=["GET", "POST"])
+async def index(requests):
+    request.app.shutdown()
+    return Response("Desligando...")
+
+app.run(port=80,debug=True)
